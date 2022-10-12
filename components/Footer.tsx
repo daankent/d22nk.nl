93014c80bcddc8018c9c0cddfd0f5c2f6d313923
D22NK <daankentrop@gmail.com>
2022-10-12 20:51:31 +0200

    Footer margin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
-    <div className="flex items-center justify-between w-full max-w-[1000px] mx-auto relative border-gray-200 pt-4 pb- sm:pb-8 text-gray-900 bg-gray-50">
-      <p className="italic text-gray-400 text-sm">
+    <div className=" flex items-center justify-between w-full max-w-[1000px] mx-auto relative border-gray-200 pt-4 pb- sm:pb-8 text-gray-900 bg-gray-50">
+      <p className="italic text-gray-400 text-sm ml-4">
         &copy; 2022-{new Date().getFullYear()} Daan Kentrop
       </p>
     </div>
